7a3c1f1c1a336a952c76a79a275d8c2ee6f5c9ee
MTrucky <146337304+MTrucky@users.noreply.github.com>
2024-03-26 09:04:38 +0500

    Update both.py

--- both.py
+++ both.py
@@ -1,49 +1,46 @@
 from time import time, sleep
 import requests
 
-def dec(fun):
+def rate_limiter(fun):
     timenow = time()
     calls = 0
-    def checks():
+
+    def decorator():
         nonlocal timenow
         nonlocal calls
-        if time() - timenow > 5:
-            timenow = time()
-            calls = 0
-        calls += 1
-        if calls <= 3:
-            return fun()
-        else:
-            print("Много")
-    return checks
 
-@dec
+        def wrapper():
+            nonlocal timenow
+            nonlocal calls
+            if time() - timenow > 5:
+                timenow = time()
+                calls = 0
+            calls += 1
+            if calls <= 3:
+                return fun()
+            else:
+                print("Много")
+        return wrapper
+    return decorator
+
+@rate_limiter
 def get_dog_fact():
-    response = requests.get('https://dogapi.dog/api/v2/facts')
-    data = response.json()
-    fact = data['data'][0]['attributes']['body']
+    last_fact = None
+
+    def fact():
+        nonlocal last_fact
+        
+        response = requests.get('https://dogapi.dog/api/v2/facts')
+        data = response.json()
+        fact = data['data'][0]['attributes']['body']
+        last_fact = {'fact': fact}
+        return last_fact['fact']
+
     return fact
 
+for _ in range(30):
+    dog_fact = get_dog_fact()
+    print(dog_fact())
+    # sleep(1)
+# print(dog_fact())
 
-dog_fact = get_dog_fact()
-print(dog_fact)
-dog_fact = get_dog_fact()
-print(dog_fact)
-dog_fact = get_dog_fact()
-print(dog_fact)
-dog_fact = get_dog_fact()
-print(dog_fact)
-sleep(6)
-dog_fact = get_dog_fact()
-print(dog_fact)
-dog_fact = get_dog_fact()
-print(dog_fact)
-dog_fact = get_dog_fact()
-print(dog_fact)
-dog_fact = get_dog_fact()
-print(dog_fact)
-
-# for _ in range(4):
-#     dog_fact = get_dog_fact()
-#     print(dog_fact)
-#     sleep(1)
